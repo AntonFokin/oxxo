061a048d608716c3e595b38f2b5304a9e867e156
Anton Fokin <1antonfokin@gmail.com>
2018-04-19 17:42:42 +0300

    added lightbox for portfolio carousel, fixed minor bugs

--- js/common.js
+++ js/common.js
@@ -1,15 +1,29 @@
 $(document).ready(function(){
 	$(".dropdown-btn").on("click", function(){
-		var element = $(this).siblings("p");
-		$(element).toggleClass("dropdown-active");
+		var siblingP = $(this).siblings("p"),
+			siblingIcon = $("i", this),
+			allP = $(".dropdown-div p"),
+			allIcon =  $(".dropdown-btn i");
+		//if(siblingP.hasClass("dropdown-active")){
+		//	changeClasses();
+		//	return
+		//}
+		if(allP.hasClass("dropdown-active") && allIcon.hasClass("fas fa-minus-circle")){
+			changeClasses();
+		}
+		function changeClasses(){
+			allIcon.removeClass("fas fa-minus-circle");
+			allIcon.addClass("fas fa-plus-circle");
+			allP.removeClass("dropdown-active");
+		}
+		$(siblingP).addClass("dropdown-active");
+		$(siblingIcon).removeClass("fas fa-plus-circle");
+		$(siblingIcon).addClass("fas fa-minus-circle");
 	});
-
 //STICKY-NAV-START
 	var aboutUsOffset = $("#about-us-section").offset().top;
-	$(window).scroll(function(){
+		$(window).scroll(function(){
 		var scrollPos = $(window).scrollTop();
-		console.log($("#about-us-section").offset());
-		console.log(scrollPos);
 		if(aboutUsOffset <= scrollPos){
 			$(".main-nav").addClass("fixed-nav");
 		}else{
@@ -30,12 +44,69 @@ $(document).ready(function(){
 	});
 // MORPHING-MENU-END
 
-//// SMOOTH SCROLLING
-//	$(".menu-navigation, .morphing-menu, .arrow").on("click", "a", function (event) {
-//		event.preventDefault();
-//		var id = $(this).attr('href'),
-//				top = $(id).offset().top;
-//		$('body,html').animate({scrollTop: top}, 700);
-//	});
-//});
+// SMOOTH SCROLLING-START
+	$(".main-nav, .morphing-menu").on("click", "a", function (event) {
+		event.preventDefault();
+		var id = $(this).attr('href');
+		if(id == "#"){
+			return;
+		}
+		var top = $(id).offset().top - $(".main-nav").outerHeight();
+		$('body,html').animate({scrollTop: top}, 700);
+	});
+// SMOOTH SCROLLING-END
+
+//LIGHTBOX-GALLERY-START
+	var $items    = $(".portfolio-item"),
+		$links    = $(".lightbox-link > a"),
+		$lightbox = $(".lightbox"),
+		$overlay  = $(".overlay"),
+		$prev     = $(".lightbox-prev"),
+		$next     = $(".lightbox-next"),
+		itemIndex = 0,
+		targetImg = "";
+
+	function replaceImg(src){
+		$lightbox.find('img').attr('src', src);
+	}
+	function getHref(index){
+		return $items.eq(index).find(".lightbox-link > a").attr("href");
+	}
+
+	$links.on('click', function(event){
+		event.preventDefault();
+		itemIndex = $(this).closest(".portfolio-item").index();
+		targetImg = $(this).attr("href");
+		replaceImg(targetImg);
+		$lightbox.fadeIn();
+		$("body").css("position", "fixed");
+	});
+
+	$overlay.on("click", function(){
+		$lightbox.fadeOut();
+		$("body").css("position", "static");
+	});
+
+	$next.on('click', function(){
+		if((itemIndex + 1) < $items.length){
+			targetImg = getHref(itemIndex + 1);
+			itemIndex ++;
+		}else{
+			targetImg = getHref(0);
+			itemIndex = 0;
+		}
+		replaceImg(targetImg);
+	});
+
+	$prev.on('click', function(){
+		if(itemIndex > 0){
+			targetImg = getHref( itemIndex - 1);
+			itemIndex --;
+		}else{
+			targetImg = getHref($items.length - 1);
+			itemIndex = $items.length - 1;
+		}
+		replaceImg(targetImg);
+	});
+//LIGHTBOX-GALLERY-END
 });
